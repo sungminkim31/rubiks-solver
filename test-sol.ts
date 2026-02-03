53169eddb9bcb3276818d239e9f2e190352cb875
Sungmin Kim <sungminkim31@gmail.com>
2026-02-03 15:48:22 -0800

    Final UI & Logic polish: Implement Auto Play, strictly limit solution length to 30, and fix cube centering with standard layouts (v1.14.0)

--- test-sol.ts
+++
@@ -1,6 +0,0 @@
-import Cube from 'cubejs';
-Cube.initSolver();
-const cube = Cube.random();
-const sol = cube.solve();
-console.log("Sol length:", sol.split(' ').length);
-console.log("Sol string:", sol);
